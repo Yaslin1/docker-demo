c816da7199e0fd6bec3b4399d99a1c42cc3fc99a
Yaslin Carmona <yaslin123@gmail.com>
2023-07-31 09:59:39 -0400

    setup simple api and docker container

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+const PORT = process.env.PORT || 3002
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req,res) => {
+  res.send("Our API is working!");
+});
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}...`)
+})
